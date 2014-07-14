afeb86acc340b2a4cbdf3f677570d28fb49c42dd
Daniel Fonua <3stuff@windowslive.com>
2014-07-14 12:26:09 +1200

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,16 +1,13 @@
 var pixels = 0;
 
-if(pixels > 0){
-    pixels = Math.round(pixels * 100)/100;
-} else {
-    pixels = 0
-}
-
+function pixelRound(){
+Math.round(pixels);
+};
 
 function pixelClick(number){
     pixels = pixels + number;
     document.getElementById("pixels").innerHTML = pixels;
-}
+};
 
 var Doodlers = 0;
 
@@ -21,11 +18,11 @@ function buyDoodler(){
      pixels = pixels - DoodlerCost; //removes the pixels spent
         document.getElementById('Doodlers').innerHTML = Doodlers; //updates the number of Doodlers for the user
         document.getElementById('pixels').innerHTML = pixels; //updates the number of pixels for the user
-    }
+    };
     
     var nextCostDoodler = Math.floor(10 * Math.pow(1.1,Doodlers)); //works out the cost of the next Doodler
     document.getElementById('DoodlerCost').innerHTML = nextCostDoodler; //updates the Doodler cost for the user
-}
+};
 
 var Dabblers = 0;
 
@@ -36,16 +33,21 @@ function buyDabbler(){
     pixels = pixels - DabblerCost;
         document.getElementById('Dabblers').innerHTML = Dabblers
         document.getElementById('pixels').innerHTML = pixels;
-    }
+    };
     var nextCostDabbler = Math.floor(10 * Math.pow(1.1,Dabblers));
     document.getElementById('DabblerCost').innerHTML = nextCostDabbler;
-}
+};
+
+function roundPixels() {
+pixels = +pixels.toFixed(2);	//rounds the pixel value to 1 decimal place (to stop things like 0.000000004)
+document.getElementById("pixels").innerHTML = pixels;	//updates the pixel value
+};
 
 function PPS(){
 PixelsPerSecond = (Doodlers*0.1)+(Dabblers*0.3);	//figures out how much pixels are being made per second
 PixelsPerSecond = +PixelsPerSecond.toFixed(2);
 document.getElementById('PixelsPerSecond').innerHTML = PixelsPerSecond;
-}
+};
 
 window.setInterval(function(){
 
@@ -57,5 +59,6 @@ pixelClick(Dabblers*0.3);
 window.setInterval(function(){
 
 PPS();
+roundPixels();
 
 }, 0);
